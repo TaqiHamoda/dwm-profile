213ac8588df7a87fb835e7ba31a254c7e07d2f0b
Taqi Hamoda <taqihisham@gmail.com>
2021-10-04 20:32:34 -0400

    Added alpha patch to dmenu

--- dmenu-5.0/config.h
+++ dmenu-5.0/config.h
@@ -2,10 +2,13 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xa0;     /* alpha value for transparency */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
 };
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
@@ -13,6 +16,13 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 9;
 
@@ -21,7 +31,6 @@ static unsigned int lines      = 9;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
-
 
 /* Size of the window border */
 static unsigned int border_width = 2;
