db80c16d7e6c143132d0134cfc2c5ce15ca7254b
Taqi Hamoda <taqihisham@gmail.com>
2021-10-09 07:58:58 -0400

    Changed the color scheme of the dwm bar

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -6,20 +6,20 @@ static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int cornerrad = 4;        /* The corner radius */
 static const unsigned int minwsz    = 20;       /* Minimal heigt of a client for smfact */
-static const unsigned int gappx     = 10;        /* gap pixel between windows */
+static const unsigned int gappx     = 10;       /* gap pixel between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int barheight	    = 1;        /* adds extra height to bar */
 static const char *fonts[]          = { "ubuntumono:size=13" };
 static const char dmenufont[]       = { "ubuntumono:size=15" };
 static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray2[]       = "#888888";
+static const char col_gray3[]       = "#f1f1f1";
+static const char col_other[]       = "#888888";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray2, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray3, col_gray1,  col_gray2  },
 };
 
 /* tagging */
@@ -71,7 +71,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray2, "-sb", col_other, "-sf", col_gray3, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
 
